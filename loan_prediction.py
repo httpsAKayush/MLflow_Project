b98a5b5ec71d694da16da0d5d64c931b9131369b
httpsAKayush <ak4895931@gmail.com>
2026-01-09 00:14:07 +0530

    remove track

--- loan_prediction.py
+++ loan_prediction.py
@@ -117,7 +117,7 @@ model_tree = grid_tree.fit(X_train, y_train)
 
 
 # MLflow
-mlflow.set_tracking_uri("http://127.0.0.1:5000")
+# mlflow.set_tracking_uri("http://127.0.0.1:5000")
 # mlflow.set_experiment("Loan_prediction")
 
 
